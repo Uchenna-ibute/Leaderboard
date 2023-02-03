109752ae41b720fc59f6226c91b9610b4ac1123d
Uche Ibute <uchennaoluchi49@gmail.com>
2023-02-03 12:45:35 +0100

    fixing the linter error

--- src/index.js
+++ src/index.js
@@ -3,35 +3,36 @@ import getScore from './modules/show.js';
 import print from './modules/print.js';
 
 const createGame = () => {
-    const create = async () => {
-      const get = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
-        {
-          method: 'post',
-          body: JSON.stringify({ name: 'uche' }),
-          headers: { 'Content-type': 'application/json' },
-        });
-      const res = await get.json();
-      return res;
-    };
-    create();
+  const create = async () => {
+    const get = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/',
+      {
+        method: 'post',
+        body: JSON.stringify({ name: 'uche' }),
+        headers: { 'Content-type': 'application/json' },
+      });
+    const res = await get.json();
+    return res;
   };
-createGame()
-const gamer = () => {
-    const name = document.querySelector('#name');
-    const score = document.querySelector('#score');
-    const add = document.querySelector('#add');
-    const reload = document.querySelector('#reload');
-
-    add.addEventListener('submit', (e) => {
-      e.preventDefault();
-      getScore(name.value, score.value);
-      name.value = '';
-      score.value = '';
-    });
-    reload.addEventListener('click', () => {
-      location.reload();
-    });
+  create();
 };
-gamer()
+createGame();
+const gamer = () => {
+  const name = document.querySelector('#name');
+  const score = document.querySelector('#score');
+  const add = document.querySelector('#add');
+  const reload = document.querySelector('#reload');
 
-print()
+  add.addEventListener('submit', (e) => {
+    e.preventDefault();
+    getScore(name.value, score.value);
+    name.value = '';
+    score.value = '';
+  });
+  reload.addEventListener('click', () => {
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  });
+};
+gamer();
+
+print();
