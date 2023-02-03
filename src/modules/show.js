11cd0e06daa4fd0d41332a60f40a38ec0f813bff
Uche Ibute <uchennaoluchi49@gmail.com>
2023-02-03 12:33:40 +0100

    Setting the API

---
+++ src/modules/show.js
@@ -0,0 +1,18 @@
+const getScore = (name, score) => {
+    const getScore = async () => {
+      const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/wsIOxR0bKz1yTE4Uisg5/scores/',
+        {
+          method: 'post',
+          body: JSON.stringify({
+            user: name,
+            score,
+          }),
+          headers: { 'Content-type': 'application/json' },
+        });
+      const response = await res.json();
+      return response;
+    };
+    getScore();
+  };
+
+  export default getScore;
